56cb40094c3fb302324938522f2d9f3ed250d8d1
Gustavo Barboza <Huedor123@gmail.com>
2022-11-26 14:51:43 -0300

    V.1.1

--- Firebase_App/app/src/main/java/com/example/password_saver/Auth/Login.kt
+++ Firebase_App/app/src/main/java/com/example/password_saver/Auth/Login.kt
@@ -35,7 +35,23 @@ class Login : AppCompatActivity() {
 
             val email = findViewById<EditText>(R.id.txt_email).text.toString()
             val senha = findViewById<EditText>(R.id.txt_senha).text.toString()
-            val path = email.split("@")[0]
+            val Email  = email.split("@")[0]
+            var word = ""
+
+            if(Email.contains(".")){
+                val email2 = email.split(".")[0]
+                val email3 = email.split(".")[1].split("@")[0]
+                word = email2 + email3
+                Log.d("valueFirebase","$word")
+
+            }else{
+                word = Email
+
+            }
+
+            val path = email.split("@")[1].split(".")[0]
+
+            val user = (word + path)
 
 
             if( email.isNotEmpty() && senha.isNotEmpty()){
@@ -44,8 +60,8 @@ class Login : AppCompatActivity() {
                         //Todo() pode dar erro aqui
 
                         val intent = Intent(this, MainActivity::class.java )
-                        Log.d("value","$path")
-                        intent.putExtra("User","${path}")
+                        Log.d("value","$user")
+                        intent.putExtra("UserFirebase","${user}")
                         startActivity(intent)
 
                     }
